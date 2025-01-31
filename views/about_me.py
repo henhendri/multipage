af38fff7190e2a5f0be939d66a6aeff6a17e7550
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-01-31 19:01:17 +0700

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.image("https://static.streamlit.io/examples/cat.jpg")
+st.image("https://static.streamlit.io/examples/cat.jpg", width=50px, height=70px)
 st.title(f"Hendri Setiadi, S.Tr.Kom., Gr.", anchor=False)
 
 # --- EXPERIENCE & QUALIFICATIONS ---
