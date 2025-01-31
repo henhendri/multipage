b4e7215f23b45b5306c1220cf51bc7ed7b763045
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-01-31 20:28:44 +0700

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+    
 st.image("https://static.streamlit.io/examples/cat.jpg", width=100)
 st.title(f"Hendri Setiadi, S.Tr.Kom., Gr.", anchor=False)
 
