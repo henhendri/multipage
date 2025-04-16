517d197eb90ddf314065f2b4cedee396b4be3f2c
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-04-16 13:05:14 +0700

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.header (":mailbox: Get in touch with Me!")
+
+contct_form = """
+<form action="https://formsubmit.co/your@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=true)
