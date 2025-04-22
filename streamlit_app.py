23000abf4359f444392bfc76d35c47b9010c3f1a
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-04-22 14:04:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ contact = st.Page(
 pg = st.navigation(
     {
         "Info": [about_page],
-        "Projects": [project_1_page, project_2_page, project_3_page],
+        "Projects": [project_1_page, project_2_page],
         "Contact": [contact]
     }
 )
