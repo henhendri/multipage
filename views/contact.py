bc8524b80f846b46da4acbb0da3b316e7c2c3974
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-04 12:45:30 +0700

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -8,16 +8,16 @@ st.title(f"Mail Message", anchor=False)
 st.page == "Contact":
 st.write("Feel free to contact us with any questions or inquiries.")
 
-contact_form = """
+contact_form_html = """
     <form action="https://formsubmit.co/your@email.com" method="POST">
          <input type="text" name="name" placeholder="Your name" required>
          <input type="email" name="email" placeholder="Your email" required>
          <textarea name="message" placeholder="Your message here"></textarea>
          <button type="submit">Send</button>
     </form>
-    """
+"""
 
-st.markdown(contact_form, unsafe_allow_html=True)
+st.markdown(contact_form_html, unsafe_allow_html=True)
 
 # You can add other contact information below the form
 st.write("Email: contact@example.com")
