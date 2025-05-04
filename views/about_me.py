829c6848d7cb5eacd989a7499c520a9d122004b3
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-04 11:44:09 +0700

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -9,7 +9,23 @@ st.write(
 )
 st.html(
     "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
+
+     <!-- Element to contain animated typing -->
+  <span id="element"></span>
+
+  <!-- Load library from the CDN -->
+  <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+  <!-- Setup and start animation! -->
+  <script>
+    var typed = new Typed('#element', {
+      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      typeSpeed: 50,
+    });
+  </script>
 )
+
+
 
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write("\n")
